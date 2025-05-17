eaef77eaac314d8c1412e8e98d28f0359f7c81ba
Andrei Lebedev <gygabyyyte@gmail.com>
2025-05-17 22:34:52 +0200

    feat: prepared Share page with form and image picker

--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -1,15 +1,50 @@
-import Link from 'next/link';
+import cl from './page.module.css';
+import ImagePicker from '@/components/ImagePicker';
 
-export default function MealsPage() {
+export default function ShareMealPage() {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>Meals Share Page</h1>
-      <p>
-        <Link href="/">Home</Link>
-      </p>
-      <p>
-        <Link href="/meals">Back</Link>
-      </p>
-    </main>
+    <>
+      <header className={cl.header}>
+        <h1>
+          Share your <span className={cl.highlight}>favorite meal</span>
+        </h1>
+        <p>Or any other meal you feel needs sharing!</p>
+      </header>
+      <main className={cl.main}>
+        <form className={cl.form}>
+          <div className={cl.row}>
+            <p>
+              <label htmlFor="name">Your name</label>
+              <input type="text" id="name" name="name" required />
+            </p>
+            <p>
+              <label htmlFor="email">Your email</label>
+              <input type="email" id="email" name="email" required />
+            </p>
+          </div>
+          <p>
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" required />
+          </p>
+          <p>
+            <label htmlFor="summary">Short Summary</label>
+            <input type="text" id="summary" name="summary" required />
+          </p>
+          <p>
+            <label htmlFor="instructions">Instructions</label>
+            <textarea
+              required
+              id="instructions"
+              name="instructions"
+              rows={10}
+            ></textarea>
+          </p>
+          <ImagePicker name="image" />
+          <p className={cl.actions}>
+            <button type="submit">Share Meal</button>
+          </p>
+        </form>
+      </main>
+    </>
   );
 }
